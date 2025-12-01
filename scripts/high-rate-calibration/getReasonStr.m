31bbcae94f6ef6e6dbb0a719cbae9e5e3395b619
August Johansson <august.johansson@sintef.no>
2025-12-01 16:07:45 +0100

    Move files into separate dirs

---
+++ scripts/high-rate-calibration/getReasonStr.m
@@ -0,0 +1,29 @@
+function reasonStr = getReasonStr(history)
+
+    if numel(history.val) == 1
+
+        warning('Only one iteration in callOptimizer');
+        valstr = sprintf('Value is %g\n', history.val(end));
+        pgstr = sprintf('Gradient value is %g\n', history.pg(end));
+
+    elseif numel(history.val) == 2
+
+        warning('Only two iterations in callOptimizer');
+        diffop = @(v, offset) abs(v(end)-v(end-1));
+        valstr = sprintf('Value diff is %g\n', diffop(history.val, 0));
+        pgstr = sprintf('Gradient diff is %g\n', diffop(history.pg, 0));
+
+    else
+
+        diffop = @(v, offset) abs(v(end-offset)-v(end-offset-1));
+        valstr = sprintf('Value diffs (prev last %g) %g\n', diffop(history.val, 1), diffop(history.val, 0));
+        pgstr = sprintf('Gradient diffs (prev last %g) %g\n', diffop(history.pg, 1), diffop(history.pg, 0));
+
+    end
+
+    reasonStr = [sprintf('Reason for termination:\n'), ...
+                 valstr, ...
+                 pgstr, ...
+                 sprintf('number of iterations %g\n', numel(history.val))];
+
+end
